857d40551bacfa298d21a9bb1bc012614ddb56d9
Laura Gyr <laura.gyr@hotmail.com>
2018-08-29 17:19:39 +0200

    filled in content

--- js/interaction.js
+++ js/interaction.js
@@ -1,5 +1,20 @@
-document.querySelectorAll("diagram").onclick = function() {clickFunction()};
+// get all diagrams in an array
+var diagrams = document.querySelectorAll(".diagram")
 
-function clickFunction() {
-    document.
+// for every diagram, add an event listener
+for(var i=0; i < diagrams.length; i++) {
+  var diagram = diagrams[i]
+  diagram.addEventListener("click", toggleData)
+}
+
+// add a class if it does not have it
+// take it away if it does
+function toggleData(e) {
+  console.log(e.target)
+  var parent = e.target.parentElement
+  if(parent.classList.contains("showdata")) {
+    parent.classList.remove("showdata")
+  } else {
+    parent.classList.add("showdata")
+  }
 }
